4fe251926353bc5ba4e36189fdda9fe67459d0dd
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-04-30 00:02:32 -0500

    Fix package installation commands in TestCode1.sh
    Updated package installation commands to use quotes for package names.

--- functions/TestCode1.sh
+++ functions/TestCode1.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+# TODO: Update Fedora detection to use dnf.
+
 YUM_PACKAGE_NAME="python python-devl python-pip openssl-devel"
 DEB_PACKAGE_NAME="python2.7 python-dev python-pip libssl-dev"
 
@@ -8,41 +10,41 @@ DEB_PACKAGE_NAME="python2.7 python-dev python-pip libssl-dev"
     echo "==============================================="
     echo "Installing packages $YUM_PACKAGE_NAME on CentOS"
     echo "==============================================="
-    yum install -y $YUM_PACKAGE_NAME
+    yum install -y "$YUM_PACKAGE_NAME"
  elif cat /etc/*release | grep ^NAME | grep Red; then
     echo "==============================================="
     echo "Installing packages $YUM_PACKAGE_NAME on RedHat"
     echo "==============================================="
-    yum install -y $YUM_PACKAGE_NAME
+    yum install -y "$YUM_PACKAGE_NAME"
  elif cat /etc/*release | grep ^NAME | grep Fedora; then
     echo "================================================"
-    echo "Installing packages $YUM_PACKAGE_NAME on Fedorea"
+    echo "Installing packages $YUM_PACKAGE_NAME on Fedora"
     echo "================================================"
-    yum install -y $YUM_PACKAGE_NAME
+    yum install -y "$YUM_PACKAGE_NAME"
  elif cat /etc/*release | grep ^NAME | grep Ubuntu; then
     echo "==============================================="
     echo "Installing packages $DEB_PACKAGE_NAME on Ubuntu"
     echo "==============================================="
     apt-get update
-    apt-get install -y $DEB_PACKAGE_NAME
+    apt-get install -y "$DEB_PACKAGE_NAME"
  elif cat /etc/*release | grep ^NAME | grep Debian ; then
     echo "==============================================="
     echo "Installing packages $DEB_PACKAGE_NAME on Debian"
     echo "==============================================="
     apt-get update
-    apt-get install -y $DEB_PACKAGE_NAME
+    apt-get install -y "$DEB_PACKAGE_NAME"
  elif cat /etc/*release | grep ^NAME | grep Mint ; then
     echo "============================================="
     echo "Installing packages $DEB_PACKAGE_NAME on Mint"
     echo "============================================="
     apt-get update
-    apt-get install -y $DEB_PACKAGE_NAME
+    apt-get install -y "$DEB_PACKAGE_NAME"
  elif cat /etc/*release | grep ^NAME | grep Knoppix ; then
     echo "================================================="
     echo "Installing packages $DEB_PACKAGE_NAME on Kanoppix"
     echo "================================================="
     apt-get update
-    apt-get install -y $DEB_PACKAGE_NAME
+    apt-get install -y "$DEB_PACKAGE_NAME"
  else
     echo "OS NOT DETECTED, couldn't install package $PACKAGE"
     exit 1;
