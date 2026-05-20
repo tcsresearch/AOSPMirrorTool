730ed1d05acae1d0c5919a477102cb0206e4b0bf
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-05-20 02:16:37 -0500

    Implement sanity check for functions file existence
    Added a sanity check function to ensure the required functions file exists before sourcing it.

--- functions/OS_Detection/DetectOS.sh
+++ functions/OS_Detection/DetectOS.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-# TODO: Adsd Sanity Check.
-source ./DetectOS.bfunc
+function SanityChecker_FuncFileExists() {
+    OSD_FuncFile="DetectOS.bfunc"
+    if [ -f "$OSD_FuncFile" ]; then
+        source "$OSD_FuncFile"
+    else
+    echo "ERROR!  Functions File $OSD_FuncFile NOT Found. Quitting..."
+    return
+    fi
+}
 
 case $(get_distro) in
     raspbian) 
