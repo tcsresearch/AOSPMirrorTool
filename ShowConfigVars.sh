b842655a0528eb06c3680c0d21c47097d2a70e9a
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-05-20 02:38:49 -0500

    Implement sanity check for config file
    Added a sanity check for the configuration file and updated the show config function.

--- ShowConfigVars.sh
+++ ShowConfigVars.sh
@@ -1,8 +1,27 @@
 #!/bin/env bash
 
-echo "AOSP Home Directory:  	$AOSP_HomeDir "
-echo "Config Dir:  		$ConfigDir    "
-echo "Config File: 		$ConfigFile   "
-echo "Functions Dir: 		$FunctionsDir "
+# Define Config File #
+CfgFile="config/AOSPMirrorTool.conf"
 
+function SanityCheck_CfgFile() {
+    if [ ! -f "$CfgFile" ]; then
+    echo "ERROR! $CfgFile NOT Found. Quiting..."
+    return
+  else
+   source $CfgFile
+  fi
+}
+
+# Run Sanity Checkr / Source Config #
+SanityCheck_CfgFile
+
+function ShowConfig() {
+  echo "AOSP Home Directory:  	$AOSP_HomeDir "
+  echo "Config Dir:  		        $ConfigDir    "
+  echo "Config File: 		        $ConfigFile   "
+  echo "Functions Dir: 		      $FunctionsDir "
+}
+
+# Show Config #
+ShowConfig
 
